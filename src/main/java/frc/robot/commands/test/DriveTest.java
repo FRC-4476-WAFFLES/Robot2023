cb7a45ade940806b8f1a0f9ab417893ba61d3a40
DriverStation4476 <driverstation@wafflesrobotics.com>
2023-02-17 21:18:47 -0500

    autonomous movement draft

--- src/main/java/frc/robot/commands/test/DriveTest.java
+++ src/main/java/frc/robot/commands/test/DriveTest.java
@@ -4,25 +4,59 @@
 
 package frc.robot.commands.test;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import static frc.robot.RobotContainer.*;
+
 
 public class DriveTest extends CommandBase {
+  private Timer timer = new Timer();
+  private double maxCurrent = 0.0;
   /** Creates a new DriveTest. */
   public DriveTest() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    driveSubsystem.resetSteerEncoders();
+    timer.reset();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(timer.get() < 5) {
+      driveSubsystem.robotDrive(1, 0, 0, false);
+    } else if(timer.get() < 10) {
+      driveSubsystem.robotDrive(-1, 0, 0, false);
+    } else if(timer.get() < 15) {
+      driveSubsystem.robotDrive(0, 1, 0, false);
+    } else if(timer.get() < 20) {
+      driveSubsystem.robotDrive(0, -1, 0, false);
+    } else if(timer.get() < 25) {
+      driveSubsystem.robotDrive(-1, 0, 0, false);
+    } else if(timer.get() < 30) {
+      driveSubsystem.robotDrive(1, 0, 0, false);
+    } else if(timer.get() < 35) {
+      driveSubsystem.robotDrive(0, -1, 0, false);
+    } else if(timer.get() < 40) {
+      driveSubsystem.robotDrive(0, 1, 0, false);
+    } else if(timer.get() < 45) {
+      driveSubsystem.robotDrive(0, 0, Math.toRadians(72), false);
+    } else if(timer.get() < 50) {
+      driveSubsystem.robotDrive(0, 0, -Math.toRadians(72), false);
+    } else if(timer.get() < 55) {
+      driveSubsystem.robotDrive(0, 0, 0, false);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    driveSubsystem.stop();
+  }
 
   // Returns true when the command should end.
   @Override
