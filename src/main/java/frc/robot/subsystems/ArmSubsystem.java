a99efa2cbebf8ac29a4ddc9d054f7e6fd464fb80
DriverStation4476 <driverstation@wafflesrobotics.com>
2023-02-19 17:17:39 -0500

    rename arm motors

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -113,100 +113,100 @@ public class ArmSubsystem extends SubsystemBase {
 
 
   private ArmState state;
-  private final CANSparkMax climb1Left;
-  private final CANSparkMax climb1Right;
-  private final CANSparkMax climb2Left;
-  private final CANSparkMax climb2Right;
-  private final CANSparkMax intakePivotLeft;
-  private final CANSparkMax intakePivotRight;
+  private final CANSparkMax shoulderLeft;
+  private final CANSparkMax shoulderRight;
+  private final CANSparkMax elbowLeft;
+  private final CANSparkMax elbowRight;
+  private final CANSparkMax wristLeft;
+  private final CANSparkMax wristRight;
   
-  private final SparkMaxPIDController climb1LeftPID;
-  private final SparkMaxPIDController climb1RightPID;
-  private final SparkMaxPIDController climb2LeftPID;
-  private final SparkMaxPIDController climb2RightPID;
-  private final SparkMaxPIDController intakePivotLeftPID;
+  private final SparkMaxPIDController shoulderLeftPID;
+  private final SparkMaxPIDController shoulderRightPID;
+  private final SparkMaxPIDController elbowLeftPID;
+  private final SparkMaxPIDController elbowRightPID;
+  private final SparkMaxPIDController wristLeftPID;
 
-  private final RelativeEncoder climb1LeftRelativeEncoder;
-  private final RelativeEncoder climb1RightRelativeEncoder;
-  private final RelativeEncoder climb2LeftRelativeEncoder;
-  private final RelativeEncoder climb2RightRelativeEncoder;
-  private final RelativeEncoder intakePivotLeftEncoder;
-  private final RelativeEncoder intakePivotRightEncoder;
+  private final RelativeEncoder shoulderLeftRelativeEncoder;
+  private final RelativeEncoder shoulderRightRelativeEncoder;
+  private final RelativeEncoder elbowLeftRelativeEncoder;
+  private final RelativeEncoder elbowRightRelativeEncoder;
+  private final RelativeEncoder wristLeftEncoder;
+  private final RelativeEncoder wristRightEncoder;
 
-  private final DutyCycleEncoder climb1LeftAbsoluteEncoder;
-  private final DutyCycleEncoder climb1RightEncoder; 
-  private final DutyCycleEncoder climb2Encoder;
-  private final DutyCycleEncoder intakePivotEncoder;
+  private final DutyCycleEncoder shoulderLeftAbsoluteEncoder;
+  private final DutyCycleEncoder shoulderRightEncoder; 
+  private final DutyCycleEncoder elbowEncoder;
+  private final DutyCycleEncoder wristEncoder;
 
   private final ThreeJointArmKinematics kinematics;
 
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
-    climb1Left = new CANSparkMax(Constants.climb1LeftConstants.motorID, MotorType.kBrushless);
-    climb1Right = new CANSparkMax(Constants.climb1RightConstants.motorID, MotorType.kBrushless);
-    climb2Left = new CANSparkMax(Constants.climb2LeftConstants.motorID, MotorType.kBrushless);
-    climb2Right = new CANSparkMax(Constants.climb2RightConstants.motorID, MotorType.kBrushless);
-    intakePivotLeft = new CANSparkMax(Constants.intakePivotLeftConstants.motorID, MotorType.kBrushless);
-    intakePivotRight = new CANSparkMax(Constants.intakePivotRightConstants.motorID, MotorType.kBrushless);
+    shoulderLeft = new CANSparkMax(Constants.shoulderLeftConstants.motorID, MotorType.kBrushless);
+    shoulderRight = new CANSparkMax(Constants.shoulderRightConstants.motorID, MotorType.kBrushless);
+    elbowLeft = new CANSparkMax(Constants.elbowLeftConstants.motorID, MotorType.kBrushless);
+    elbowRight = new CANSparkMax(Constants.elbowRightConstants.motorID, MotorType.kBrushless);
+    wristLeft = new CANSparkMax(Constants.wristLeftConstants.motorID, MotorType.kBrushless);
+    wristRight = new CANSparkMax(Constants.wristRightConstants.motorID, MotorType.kBrushless);
 
-    climb1Left.restoreFactoryDefaults();
-    climb1Right.restoreFactoryDefaults();
-    climb2Left.restoreFactoryDefaults();
-    climb2Right.restoreFactoryDefaults();
-    intakePivotLeft.restoreFactoryDefaults();
-    intakePivotRight.restoreFactoryDefaults();
+    shoulderLeft.restoreFactoryDefaults();
+    shoulderRight.restoreFactoryDefaults();
+    elbowLeft.restoreFactoryDefaults();
+    elbowRight.restoreFactoryDefaults();
+    wristLeft.restoreFactoryDefaults();
+    wristRight.restoreFactoryDefaults();
 
-    climb1Left.setSmartCurrentLimit(Constants.climb1LeftConstants.currentLimit);
-    climb1Right.setSmartCurrentLimit(Constants.climb1RightConstants.currentLimit);
-    climb2Left.setSmartCurrentLimit(Constants.climb2LeftConstants.currentLimit);
-    climb2Right.setSmartCurrentLimit(Constants.climb2RightConstants.currentLimit);
-    intakePivotLeft.setSmartCurrentLimit(Constants.intakePivotLeftConstants.currentLimit);
-    intakePivotRight.setSmartCurrentLimit(Constants.intakePivotRightConstants.currentLimit);
+    shoulderLeft.setSmartCurrentLimit(Constants.shoulderLeftConstants.currentLimit);
+    shoulderRight.setSmartCurrentLimit(Constants.shoulderRightConstants.currentLimit);
+    elbowLeft.setSmartCurrentLimit(Constants.elbowLeftConstants.currentLimit);
+    elbowRight.setSmartCurrentLimit(Constants.elbowRightConstants.currentLimit);
+    wristLeft.setSmartCurrentLimit(Constants.wristLeftConstants.currentLimit);
+    wristRight.setSmartCurrentLimit(Constants.wristRightConstants.currentLimit);
 
-    climb1Left.setIdleMode(IdleMode.kBrake);
-    climb1Right.setIdleMode(IdleMode.kBrake);
-    climb2Left.setIdleMode(IdleMode.kBrake);
-    climb2Right.setIdleMode(IdleMode.kBrake);
-    intakePivotLeft.setIdleMode(IdleMode.kBrake);
-    intakePivotRight.setIdleMode(IdleMode.kBrake);
+    shoulderLeft.setIdleMode(IdleMode.kBrake);
+    shoulderRight.setIdleMode(IdleMode.kBrake);
+    elbowLeft.setIdleMode(IdleMode.kBrake);
+    elbowRight.setIdleMode(IdleMode.kBrake);
+    wristLeft.setIdleMode(IdleMode.kBrake);
+    wristRight.setIdleMode(IdleMode.kBrake);
 
-    climb1Left.setControlFramePeriodMs(40);
-    climb1Right.setControlFramePeriodMs(40);
-    climb2Left.setControlFramePeriodMs(40);
-    climb2Right.setControlFramePeriodMs(40);
-    intakePivotLeft.setControlFramePeriodMs(40);
-    intakePivotRight.setControlFramePeriodMs(40);
+    shoulderLeft.setControlFramePeriodMs(40);
+    shoulderRight.setControlFramePeriodMs(40);
+    elbowLeft.setControlFramePeriodMs(40);
+    elbowRight.setControlFramePeriodMs(40);
+    wristLeft.setControlFramePeriodMs(40);
+    wristRight.setControlFramePeriodMs(40);
 
-    climb1Left.setInverted(Constants.climb1LeftConstants.isInverted);
-    climb1Right.setInverted(Constants.climb1RightConstants.isInverted);
-    climb2Left.setInverted(Constants.climb2LeftConstants.isInverted);
-    climb2Right.setInverted(Constants.climb2RightConstants.isInverted);
-    intakePivotLeft.setInverted(Constants.intakePivotLeftConstants.isInverted);
+    shoulderLeft.setInverted(Constants.shoulderLeftConstants.isInverted);
+    shoulderRight.setInverted(Constants.shoulderRightConstants.isInverted);
+    elbowLeft.setInverted(Constants.elbowLeftConstants.isInverted);
+    elbowRight.setInverted(Constants.elbowRightConstants.isInverted);
+    wristLeft.setInverted(Constants.wristLeftConstants.isInverted);
 
-    intakePivotRight.follow(intakePivotLeft, Constants.intakePivotRightConstants.isInverted);
+    wristRight.follow(wristLeft, Constants.wristRightConstants.isInverted);
 
-    climb1LeftPID = climb1Left.getPIDController();
-    climb1RightPID = climb1Right.getPIDController();
-    climb2LeftPID = climb2Left.getPIDController();
-    climb2RightPID = climb2Right.getPIDController();
-    intakePivotLeftPID = intakePivotLeft.getPIDController();
+    shoulderLeftPID = shoulderLeft.getPIDController();
+    shoulderRightPID = shoulderRight.getPIDController();
+    elbowLeftPID = elbowLeft.getPIDController();
+    elbowRightPID = elbowRight.getPIDController();
+    wristLeftPID = wristLeft.getPIDController();
 
-    climb1LeftRelativeEncoder = climb1Left.getEncoder();
-    climb1RightRelativeEncoder = climb1Right.getEncoder();
-    climb2LeftRelativeEncoder = climb2Left.getEncoder();
-    climb2RightRelativeEncoder = climb2Right.getEncoder();
-    intakePivotLeftEncoder = intakePivotLeft.getEncoder();
-    intakePivotRightEncoder = intakePivotRight.getEncoder();
+    shoulderLeftRelativeEncoder = shoulderLeft.getEncoder();
+    shoulderRightRelativeEncoder = shoulderRight.getEncoder();
+    elbowLeftRelativeEncoder = elbowLeft.getEncoder();
+    elbowRightRelativeEncoder = elbowRight.getEncoder();
+    wristLeftEncoder = wristLeft.getEncoder();
+    wristRightEncoder = wristRight.getEncoder();
 
-    climb1LeftAbsoluteEncoder = new DutyCycleEncoder(Constants.climb1LeftEncoder);
-    climb1RightEncoder = new DutyCycleEncoder(Constants.climb1RightEncoder);
-    climb2Encoder = new DutyCycleEncoder(Constants.climb2LeftEncoder);
-    intakePivotEncoder = new DutyCycleEncoder(Constants.intakePivotEncoder);
+    shoulderLeftAbsoluteEncoder = new DutyCycleEncoder(Constants.shoulderLeftEncoder);
+    shoulderRightEncoder = new DutyCycleEncoder(Constants.shoulderRightEncoder);
+    elbowEncoder = new DutyCycleEncoder(Constants.elbowLeftEncoder);
+    wristEncoder = new DutyCycleEncoder(Constants.wristEncoder);
 
-    climb1LeftAbsoluteEncoder.setDistancePerRotation(360);
-    climb1RightEncoder.setDistancePerRotation(360);
-    climb2Encoder.setDistancePerRotation(360);
-    intakePivotEncoder.setDistancePerRotation(360);
+    shoulderLeftAbsoluteEncoder.setDistancePerRotation(360);
+    shoulderRightEncoder.setDistancePerRotation(360);
+    elbowEncoder.setDistancePerRotation(360);
+    wristEncoder.setDistancePerRotation(360);
 
     kinematics = new ThreeJointArmKinematics(Constants.ArmConstants.a1, Constants.ArmConstants.a2, Constants.ArmConstants.a3);
     // kinematics.addConstraint(
@@ -223,21 +223,24 @@ public class ArmSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    Rotation2d currentQ1 = Rotation2d.fromDegrees(climb1LeftRelativeEncoder.getPosition() * 360 * Constants.climb1LeftConstants.ratio);
-    Rotation2d currentQ2 = Rotation2d.fromDegrees(climb2LeftRelativeEncoder.getPosition() * 360 * Constants.climb2LeftConstants.ratio);
-    Rotation2d currentQ3 = Rotation2d.fromDegrees(intakePivotLeftEncoder.getPosition() * 360 * Constants.intakePivotLeftConstants.ratio).plus(currentQ2).plus(currentQ1);
+    Rotation2d currentQ1 = Rotation2d.fromDegrees(shoulderLeftRelativeEncoder.getPosition() * 360 * Constants.shoulderLeftConstants.ratio);
+    Rotation2d currentQ2 = Rotation2d.fromDegrees(elbowLeftRelativeEncoder.getPosition() * 360 * Constants.elbowLeftConstants.ratio);
+    Rotation2d currentQ3 = Rotation2d.fromDegrees(wristLeftEncoder.getPosition() * 360 * Constants.wristLeftConstants.ratio).plus(currentQ2).plus(currentQ1);
 
     Pose2d currentPose = kinematics.toPose2d(new ThreeJointArmState(currentQ1, currentQ2, currentQ3));
     SmartDashboard.putNumber("/arm/pose/x", currentPose.getX());
     SmartDashboard.putNumber("/arm/pose/y", currentPose.getY());
     SmartDashboard.putNumber("/arm/pose/angle", currentPose.getRotation().getDegrees());
 
+    // TODO: here we should put the current state into the setPoints map to get the next target. If we are in 
+    // fudge mode, we will disregard these and set the speed and direction of the motors directly based on the operator joysticks.
+    // the state also has a variable to track the elbow vs wrist movement.
     // ThreeJointArmState targetArmState = kinematics.toArmState(targetPos);
     
-    SmartDashboard.putNumber("Left 1 encoder", climb1LeftAbsoluteEncoder.getDistance());
-    SmartDashboard.putNumber("Right 1 encoder", climb1RightEncoder.getDistance());
-    SmartDashboard.putNumber("Left 2 Encoder", climb2Encoder.getDistance());
-    SmartDashboard.putNumber("Wrist Encoder", intakePivotEncoder.getDistance());
+    SmartDashboard.putNumber("Left 1 encoder", shoulderLeftAbsoluteEncoder.getDistance());
+    SmartDashboard.putNumber("Right 1 encoder", shoulderRightEncoder.getDistance());
+    SmartDashboard.putNumber("Left 2 Encoder", elbowEncoder.getDistance());
+    SmartDashboard.putNumber("Wrist Encoder", wristEncoder.getDistance());
   }
 
   public void updateSideLeft() {
