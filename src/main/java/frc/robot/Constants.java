bbea5383c4855dbb0e652dc8eb898fd21baf968c
DriverStation4476 <driverstation@wafflesrobotics.com>
2023-04-04 20:56:12 -0400

    Made cone high setpoint legal

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -248,7 +248,7 @@ public final class Constants {
 
     public static final HashMap<ArmState, SetPoint> setPoints = new HashMap<ArmState, SetPoint>() {{
       put(new ArmState(Height.SCORE_HIGH, GamePiece.CUBE, false), new SetPoint(7000, -111000, 10));
-      put(new ArmState(Height.SCORE_HIGH, GamePiece.CONE, false), new SetPoint(14500, -150000, 30)); 
+      put(new ArmState(Height.SCORE_HIGH, GamePiece.CONE, false), new SetPoint(12000, -150000, 30)); // 14500, -150000, 30
       put(new ArmState(Height.SCORE_MEDIUM, GamePiece.CUBE, false), new SetPoint(0, -88000, 6));     
       put(new ArmState(Height.SCORE_MEDIUM, GamePiece.CONE, false), new SetPoint(-7000, -100000, 24)); 
       put(new ArmState(Height.SCORE_LOW, GamePiece.CUBE, false), new SetPoint(17000, -21000, 9.2)); 
